519b1d418b419a053364f12a283256f90d58e80a
Alumno <Alumno@PC30>
2019-09-04 12:27:59 -0500

    1.1

---
+++ menu/menu/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SalirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AcercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Este programa fue creado por Jayro Murguía", "Acerca de...MeMenu20", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void borrarColor()
+        {
+            rojoToolStripMenuItem.Checked = false;
+            azulToolStripMenuItem.Checked = false;
+        }
+
+        private void AzulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            borrarColor();
+            label1.ForeColor = Color.Blue;
+            azulToolStripMenuItem.Checked = true;
+        }
+
+        private void RojoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            borrarColor();
+            label1.ForeColor = Color.Red;
+            rojoToolStripMenuItem.Checked = true;
+        }
+
+        private void TimesNewRomanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comicSansToolStripMenuItem.Checked = false;
+            label1.Font = new Font("Time New Roman", 16, label1.Font.Style);
+            timesNewRomanToolStripMenuItem.Checked = true;
+        }
+
+        private void ComicSansToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timesNewRomanToolStripMenuItem.Checked = false;
+            label1.Font = new Font("Comic Sans MS", 16, label1.Font.Style);
+            comicSansToolStripMenuItem.Checked = true;
+        }
+    }
+}
